8436c6de51a70845ab52917fe443b14b32b0d750
un57gcder1 <un57gcder1@gmail.com>
2025-08-14 15:51:29 -0500

    progress; may need to refactor while loop or find way to do immediate reward (when game ends)

--- vi-tictactoe.py
+++ vi-tictactoe.py
@@ -13,17 +13,29 @@ def play(game):
     else:
         other = "X"
 
+    previous_turn = False
+    turn_info = [] # [tuple: init-state, int: action, tuple: dest-state, int: immediate reward]
+                   # should refresh only every player turn (not while loop iter)
+                   # using previous_turn bool
     while game.end() == False:
         state = game.board[:]
         state.append(player)
         state = tuple(state)
+        found = False
         if game.turn() == player:
+            if previous_turn == True:
+                # need to append dest-state, immediate reward to turn_info
+                # and add info to dictionaries rt, tt, & vt
+                turn_info.append(state) # append dest-state
+                # find out way to append immediate reward if game is over smh
+            turn_info = []
+            turn_info.append(state) # append init_state
             possible_actions = game.possible()
             for p in possible_actions:
                 state_plus = state + (p,)
                 if state_plus in tt.keys():
-                    pass # do more edits
-                else:
-                    found = False
-                    continue
-
+                    found = True # do more edits: choose & append action to turn_info
+            if found == False:
+                action = random.choice(possible_actions)
+                turn_info.append(action)
+            previous_turn = True
